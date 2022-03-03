f70ed8a576dbb289f27ac768f7134ef65a310c03
Wojciech Bukala <263479@student.pwr.edu.pl>
2022-03-03 19:35:59 +0100

    uzupelnione

--- z1/rownanie_cpp/rownanie.cpp
+++ z1/rownanie_cpp/rownanie.cpp
@@ -35,16 +35,7 @@ void WprowadzWspTrojmianu( TrojmianKw *wTKw )
 
 void WyswietlTrojmian( TrojmianKw TKw )
 {
-  cout<<"Trojmian: " << TKw.a<<"*x^2 + " << TKw.b << "*x + " << TKw.c << "\n";
-  /*
-   * Tu uzupelnij
-   *
-   *  Nalezy wyswietlic trojmian w postaci:
-   *    wsp_a*x^2 + wsp_b*x + wsp_c
-   *
-   *  gdzie w miejscu wspolczynnikow: wsp_a, wsp_b, wsp_c;
-   *  powinny byc wyswietlone wartosci pol TKw.a, TKw.b, TKw.c 
-   */
+  cout<<"Trojmian: " << TKw.a<<"*x^2 + " << TKw.b << "*x + " << TKw.c << "\n"; // Uzupełniono
 }
 
 
@@ -69,13 +60,8 @@ T_IloscPierwiastkow WyliczPierwiastki(TrojmianKw   TKw, double *wX1, double *wX2
   if (Delta_2 > 0) {
 
       Delta = sqrt( Delta_2 );
-      *wX1 = (-TKw.b - Delta) / (2*TKw.a);
-      *wX2 = (-TKw.b + Delta) / (2*TKw.a);
-      /*
-       * Tu uzupelnic
-       *
-       *  W analogiczny sposob nalezy wyliczyc wartosc drugiego pierwiastka.
-       */
+      *wX1 = (-TKw.b - Delta) / (2*TKw.a); 
+      *wX2 = (-TKw.b + Delta) / (2*TKw.a); //Uzupełniono
       return IP_dwa;
 
   } else if  (Delta_2 == 0) {
@@ -101,13 +87,7 @@ int main() {
   WprowadzWspTrojmianu(&TKw);
 
   if (TKw.a == 0) {
-    cout<<"Nie jest to trojmian kwadratowy\n";
-    /*
-     * Tu uzupelnic
-     *
-     *  Wpisz instrukcje informujace informujace, ze nie jest
-     *  to trojmian kwadratowy.
-     */
+    cout<<"Nie jest to trojmian kwadratowy\n"; // Uzupełniono
     return 1;
   }
 
@@ -117,33 +97,15 @@ int main() {
     case IP_dwa:
       cout << "Istnieja dwa rozwiazania rzeczywiste:\n";
       cout << "  x1 = " << X1 << endl;
-      cout << "  x1 = " << X2 << endl;
-      /*
-       * Tu uzupelnic
-       *
-       *  W analogiczny sposob wpisz instrukcje wyswietlajaca
-       *  wartosci drugiego pierwiastka.
-       */
+      cout << "  x1 = " << X2 << endl; // Uzupełniono
       break;
 
     case IP_jeden:
-    cout << "Istnieje jedno rozwiazanie rzeczywiste:\n x0 = "<< X1 <<endl;
-      /*
-       * Tu uzupelnic
-       *
-       *  Wpisz instrukcje wyswietlajaca
-       *  wartosci pierwiastka.
-       */
+    cout << "Istnieje jedno rozwiazanie rzeczywiste:\n x0 = "<< X1 <<endl; // Uzupełniono
       break;
 
     case IP_brak:
-    cout<< "Brak pierwiastkow rzeczywistych rownania"<< endl;
-      /*
-       * Tu uzupelnic
-       *
-       *  Wpisz instrukcje wyswietlajace komunikat
-       *  o braku pierwiastkow.
-       */
+    cout<< "Brak pierwiastkow rzeczywistych rownania"<< endl; // Uzupełniono
       break;
   }
 
