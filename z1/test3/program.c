a2ffbb2ececcc94705f7de9884a6f05468ff5881
Wojciech Bukala <263479@student.pwr.edu.pl>
2022-03-08 23:16:57 +0100

    08.03.2022/23:16

--- z1/test3/program.c
+++ z1/test3/program.c
@@ -1,13 +1,17 @@
+/* plik modul.c nie jest potrzebny w
+przypadku gdy zmienimy extern na static
+w deklaracji stałych pi  i e 
+stałe zostały po prostu przeniesione do maina */
+
 #include <stdio.h>
 
-
-extern const double PI;
-extern const double E;
+static const double PI = 3.141593;
+static const double E  = 2.718282;
 
 
 int main()
 {
-  printf("  PI: %f\n",PI);
-  printf("   E: %f\n",E);
+  printf("  PI: %lf\n",PI);
+  printf("   E: %lf\n",E);
   return 0;
 }
